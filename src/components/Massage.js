bf6a98f7de12d0ed4b31c7e2f95b27460d48b78e
Alex Visconde <andres.visconde@gmail.com>
2021-03-08 11:37:18 -0500

    first commit

---
+++ src/components/Massage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Massage = props => {
+
+  return(
+    <div>
+      <h2>React Boiler Plate</h2>
+    </div>
+  );
+};
+
+export default Massage;
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// main.js
+import './app.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Massage from './components/Massage';
+
+
+ReactDOM.render(
+  <Massage />,
+  document.getElementById('app')
+);
